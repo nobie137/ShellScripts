f845ce759cfd72eeca44aeb1e76e2030e6c43d06
S3lzIGZhZ2dvdAo= <109665205+nobie137@users.noreply.github.com>
2022-07-20 12:08:55 +0000

    Change the dir if you don't like it

---
+++ emotica.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+cat /home/nobie/txt/emotica | dmenu -nb "#333333" -sb "#444444" -p "Select an emoticon" -i -l 20 | awk '{print $1}' | tr -d "\t" | xclip -selection "clipboard"
+
+notify-send -i /dev/null -u "low" "Successfuly coppied to your clipboard!"
