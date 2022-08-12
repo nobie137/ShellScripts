26f61d3cfbfc4e45f6277ea532b7b4210a6b9c4a
S3lzIGZhZ2dvdAo= <109665205+nobie137@users.noreply.github.com>
2022-08-12 18:49:15 +0000

    Update and rename logout to logout.sh

--- logout.sh
+++ logout.sh
@@ -1,14 +1,14 @@
-#! /bin/sh
+#! /bin/bash
 
 choice=$(echo -e "[Logout]\n[Shutdown]\n[Reboot]" | dmenu -nb "#333333" -sb "#ff0000" -i -p  "Choose an option")
 #echo $choice
 if [ $choice == "[Reboot]" ]; then
-  systemctl reboot
+  reboot
 elif [ $choice == "[Logout]" ]; then
   #pkill -KILL -u $USER
   pkill dwm
 elif [ $choice == "[Shutdown]" ]; then
-  systemctl poweroff
+  sudo poweroff
 else
   echo "Process exited."
 fi
